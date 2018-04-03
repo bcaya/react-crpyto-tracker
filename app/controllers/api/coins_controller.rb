cdaf173168a756e1f7f945ea23220bbe60f87b62
Bobby Caya <bob.caya1@gmail.com>
2018-04-03 12:07:27 -0600

    Add api call information and httparty

---
+++ app/controllers/api/coins_controller.rb
@@ -0,0 +1,40 @@
+class Api::CoinsController < ApplicationController
+  before_action :authenticate_user! 
+  before_action :set_coin, only: [:show, :update, :destroy] 
+  BASE_URL = 'https://api.coinmarketcap.com/v1/ticker/' 
+  def index
+    render json: current_user.coins 
+  end
+
+  def create
+    cmc_id = [:coin].downcase 
+    res = HTTParty.get("#{BASE_URL}#{cmc_id}")
+    if coin = Coin.create_by_cmc_id(res)
+      watched = WatchedCoin.find_or_create_by(
+        coin_id: coin.id, 
+        user_id: current_user.id 
+      )
+      watched.update(initial_price: coin.price) if watched.initial_price.nil? 
+      render json: coin 
+    else 
+      render json: { errors: 'Coin Not Found' }, status::422 
+    #find out if the coin exists 
+    # create a coin in our database(or find it)
+    #create a watched_coin record 
+    #render back on 
+  end
+
+  def show
+    render json: @coin 
+  end
+#PUT /api/coins/:id 
+  def update
+    current_user
+      .watched_coins 
+      .find_by(coin_id: @coin.id)
+      .destroy 
+  end
+
+  def destroy
+  end
+end
